5153e6f8cf3bdaf57ecbaf2b5bc70a5e017837c8
Jm <jmdelasalas.dev@gmail.com>
2024-10-19 00:09:13 +0800

    Added navigation and imported icons

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div id="app">
+    <Header v-if="isLoggedIn && !loading" :user="user" />
     <router-view />
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+import Header from "./components/Navigation/Header.vue";
 export default {
   name: "App",
+  components: {
+    // eslint-disable-next-line vue/no-reserved-component-names
+    Header,
+  },
+  computed: {
+    ...mapState(["userDetails", "loading"]),
+    isLoggedIn() {
+      return this.userDetails !== null;
+    },
+  },
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,22 @@
 import "./assets/main.css";
 import { createApp } from "vue";
 
-import ElementPlus from 'element-plus'
+import ElementPlus from "element-plus";
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 import App from "./App.vue";
 
 import router from "./router";
 import store from "./store";
 
-
 const app = createApp(App);
 
-app.use(ElementPlus)
+app.use(ElementPlus);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
 app.use(router);
 app.use(store);
 app.mount("#app");
